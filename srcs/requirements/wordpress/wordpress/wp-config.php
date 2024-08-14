ebc97af47432c0ddbdb071421b71fb7f21f33475
resaito <resaito@student.42tokyo.jp>
2024-08-14 17:23:04 +0900

    phpとdbを接続

--- srcs/requirements/wordpress/wordpress/wp-config.php
+++ srcs/requirements/wordpress/wordpress/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'user01' );
 define( 'DB_PASSWORD', 'password' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'db' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
