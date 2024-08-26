10a3711d3fee9ee941d19456edec2a2986171fdd
resaito <resaito@student.42tokyo.jp>
2024-08-26 21:41:36 +0900

    スクリプトの内容を環境変数に変更

--- srcs/requirements/wordpress/wordpress.sh
+++ srcs/requirements/wordpress/wordpress.sh
@@ -1,23 +1,26 @@
 #! /bin/bash
 
-wp core download --allow-root --locale=ja --path=/var/www/wordpress
-rm -rf /var/www/wordpress/wp-config.php && cd /var/www/wordpress
-wp core config --allow-root \
-               --dbname=$MYSQL_DATABASE \
-               --dbuser=$MYSQL_USER \
-               --dbpass=$MYSQL_PASSWORD \
-               --dbhost=db
+cd /var/www/wordpress
+# rm -rf /var/www/wordpress/wp-config.php && cd /var/www/wordpress
+if [ ! -f wp-config.php ]; then
+    wp core download --allow-root --locale=ja --path=/var/www/wordpress
+    wp core config --allow-root \
+                --dbname=$MYSQL_DATABASE \
+                --dbuser=$MYSQL_USER \
+                --dbpass=$MYSQL_PASSWORD \
+                --dbhost=db
+fi
 
 wp core install --allow-root \
-                --title='test' \
-                --admin_user='test_user' \
-                --admin_password='password' \
+                --title=$WORDPRESS_TITLE \
+                --admin_user=$WORDPRESS_ADMIN_USER \
+                --admin_password=$WORDPRESS_ADMIN_PASS \
                 --admin_email='croodofkoga@gmail.com' \
                 --url=http://resaito.42.fr
 
 wp user create --allow-root \
-                resaito \
+                $WORDPRESS_USER \
                 resaito@student.42tokyo.jp \
-                --user_pass="password"
+                --user_pass=$WORDPRESS_USER_PASS
 
 exec "$@"
