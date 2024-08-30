7eb76ef200a518bd227e0dfdddace58dac660bb8
resaito <resaito@student.42tokyo.jp>
2024-08-30 10:30:05 +0900

    コメントアウトの削除

--- srcs/requirements/mariadb/docker-entrypoint.sh
+++ srcs/requirements/mariadb/docker-entrypoint.sh
@@ -1,28 +1,11 @@
 #! /bin/bash
 
-# service mysql start
 service mariadb start
-
-# sleep 15
-
-# if [ "$MYSQL_USER" -a "$MYSQL_PASSWORD" ]; then
-#     echo "hoge"
-
-# 	echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" | "${mysql[@]}"
-
-# 	if [ "$MYSQL_DATABASE" ]; then
-# 		echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' ;" | "${mysql[@]}"
-# 	fi
-
-# 	echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
-# fi
 
 mysql -u root -e "create database if not exists $MYSQL_DATABASE;"
 mysql -u root -e "create user '$MYSQL_USER'@'%' identified by '$MYSQL_PASSWORD';"
 mysql -u root -e "grant all on $MYSQL_DATABASE.* to '$MYSQL_USER'@'%';"
-# mysql -u root wordpress < file.sql
 
-# service mysql stop
 service mariadb stop
 
 exec "$@"
